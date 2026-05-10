b2f372afc65388e78060b0251f0efe4d2eeba84d
Ayush Raj <ap4ayush@gmail.com>
2026-05-10 19:56:09 +0530

    Completed remaining assignment questions

--- Assignment_7_SOLUTION.py
+++ Assignment_7_SOLUTION.py
@@ -112,6 +112,7 @@ match x:
 """
 
 #4
+"""
 x = int(input("Enter age "))
 match x:
     case x if x<10:
@@ -121,11 +122,145 @@ match x:
     case x if x>=20 and x<40:
                print("you are young")
     case x if x<60 and x>=40:
-               print("you are an experienced")
+               print("you are experienced")
     case x if x>=60:
-               print("you are a senior citizen")
-    case _:
-          print("invalid")
-               
+                print("you are an senior citizen")
+    case _ :
+                print("invalid")
+"""
+#5 
+'''Write a program which takes a number from user. Print Saurabh Shukla if the number
+is even, print Prateek Jain if the number is negative odd number and print Aditya
+Choudhary if number is positive odd number.
+ '''
+"""
+num = int(input("enter a number"))
+if num % 2 == 0:
+    print("Saurabh Shukla")
+elif num > 0:
+    print("Aditya Choudhary")
+else:
+    print("Prateek Jain")
+"""
 
-   
+#6 
+'''
+Write a python program to check whether a given string is a multiword string or single
+word string using match case statement
+
+'''
+"""
+s = input(" enter a string : ")
+match s:
+    case s if ' ' in s:
+        print("Multiword string")
+    case s if ' ' not in s:
+        print("Single Word string")
+
+"""       
+#7
+'''
+Write a python program to check whether a given number is positive, negative or
+zero using match case statement
+'''
+"""
+num = int(input("Enter a number : "))
+match num:
+    case num if num>0:
+        print("Positive")
+    case num if num<0:
+        print("negative")
+    case num if num == 0:
+        print("Zero")
+
+"""
+
+#8
+'''
+Write a python script to check whether two given strings are identical, first string
+comes before the second in dictionary order or first string comes after the second
+string in dictionary order using match case statement
+'''
+"""
+s1= input("Enter 1st string")
+s2= input("Enter 2st string")
+match(s1,s2):
+    case (s1,s2) if s1==s2:
+        print("identical")
+    case(s1,s2) if s1<s2:
+        print("{} comes before {}".format(s1,s2))
+    case(s1,s2) if s1>s2:
+        print("{} comes before {}".format(s2,s1))
+
+"""
+#9
+'''
+Write a python script to check whether a given year is
+a. Non century leap year
+b. Century leap year
+c. Non century non leap year
+d. Century non leap year
+'''
+"""
+year = int(input(" Enter a year"))
+if year%100 == 0:
+    if year%400 == 0:
+        print("Century leap year")
+    else:
+        print("century non-leap year")
+elif year%4 == 0:
+    print("Non-century leap year")
+else:
+    print("Non-century non leap year")
+"""
+#output:
+'''
+ Enter a year2000
+Century leap year
+Enter a year2016
+Non-century leap year
+Enter a year1999
+Non-century non leap year
+Enter a year1700
+century non-leap year
+'''
+#10.
+'''
+Write a program to display day name on the basis of user’s liking of a colour. Ask
+user for his favorite colour. User can answer in a sentence like “I like red colour”.
+Assuming all colour name entered by user is in lowercase. Use match case to display
+day name associated with the colour.
+a. Yellow - Monday
+b. Blue - Tuesday
+c. Orange - Wednesday
+d. White - Thursday
+e. Black - Friday
+f. Red - Saturday
+g. All other colours - Sunday
+'''
+
+"""
+s1 = input(" what's your favorite colour?").lower()          
+l1 = ["yellow","blue","orange","white","black","red"]
+for colour in l1:
+    if colour in s1:
+        c=colour
+        break
+    else:
+        c="other"
+match c :
+    case "yellow" :
+        print("Monday")
+    case "blue":
+        print("Tuesday")
+    case "orange":
+        print("Wednesday")
+    case "white":
+        print("Thursday")
+    case "black":
+        print("Friday")
+    case "red":
+        print("Saturday")
+    case other:
+        print("Sunday")
+"""
