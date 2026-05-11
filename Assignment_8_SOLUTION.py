80438c036ba1fe212e4feb9341f88db37bb6a748
Ayush Raj <ap4ayush@gmail.com>
2026-05-11 22:52:09 +0530

    Completed assignment 8 on while loops

---
+++ Assignment_8_SOLUTION.py
@@ -0,0 +1,91 @@
+"""
+1. Write a python script to print MySirG 5 times on the screen
+2. Write a python script to print first 10 natural numbers
+3. Write a python script to print first 10 natural numbers in reverse order
+4. Write a python script to print first 10 odd natural numbers
+5. Write a python script to print first 10 odd natural numbers in reverse order
+6. Write a python script to print first 10 even natural numbers
+7. Write a python script to print first 10 even natural numbers in reverse order
+8. Write a python script to print squares of first 10 natural numbers
+9. Write a python script to print cubes of first 10 natural numbers
+10. Write a python script to print first 10 multiples of 5
+"""
+
+#1 Write a python script to print MySirG 5 times on the screen
+"""
+i=1
+while i<=5:
+    print("MySirG")
+    i+=1
+"""
+#2 Write a python script to print first 10 natural numbers
+"""
+i=1
+print("First 10 natural number is :")
+while i<=10:
+    print(i, end=' ')
+    i+=1
+"""
+#3 Write a python script to print first 10 natural numbers in reverse order
+"""
+i=10
+print("First 10 natural number in reverse order is :")
+while i>=1:
+    print(i, end=' ')
+    i-=1
+"""
+#4 Write a python script to print first 10 odd natural numbers
+"""
+i=1
+print("first 10 odd natural numbers is :")
+while i<=20:
+    print(i, end = ' ')
+    i+=2
+"""
+#5 Write a python script to print first 10 odd natural numbers in reverse order
+"""
+i=19
+print("first 10 odd natural numbers in reverse order is :")
+while i>=1:
+    print(i,end=' ')
+    i-=2
+"""
+#6 Write a python script to print first 10 even natural numbers
+"""
+i=2
+print("First 10 even natural number is :")
+while i<=20:
+    print(i, end=' ')
+    i+=2
+"""
+#7 Write a python script to print first 10 even natural numbers in reverse order
+"""
+i=20
+print("First 10 even natural number in reverse order is :")
+while i>=2:
+    print(i, end=' ')
+    i-=2
+"""
+#8. Write a python script to print squares of first 10 natural numbers
+"""
+i=1
+print("squares of first 10 natural numbers is :")
+while i<=10:
+    print(i**2, end=' ')
+    i+=1
+"""
+#9. Write a python script to print cubes of first 10 natural numbers
+"""
+i=1
+print("cube of First 10 natural number is :")
+while i<=10:
+    print(i**3, end=' ')
+    i+=1
+"""
+#10 Write a python script to print first 10 multiples of 5
+
+i=1
+print("first 10 multiples of 5 :")
+while i<=10:
+    print(i*5, end=' ')
+    i+=1
